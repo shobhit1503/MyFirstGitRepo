74f3576d5784a7581b1e07b7d88666d8155145d0
Shobhit Deshwal <shobhitdeshwal1503@gmail.com>
2020-05-10 01:55:31 +0530

    hello world added

---
+++ tst.py
@@ -0,0 +1,11 @@
+String1 = "GeeksFor Geeks"
+print("Initial String: ")  
+print(String1)  
+    
+# Printing First character  
+print("\nFirst character of String is: ")  
+print(String1[0])  
+    
+# Printing Last character  
+print("\nLast character of String is: ")  
+print(String1[-6]) 
